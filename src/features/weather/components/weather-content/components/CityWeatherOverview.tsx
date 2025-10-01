4763aa807cf2dc875e9b58c34c5a2013eeb67648
rsmajlov <rijad.smajlovic@dccs.eu>
2025-10-01 19:15:39 +0200

    Refactor CityWeatherOverview component

--- src/features/weather/components/weather-content/components/CityWeatherOverview.tsx
+++ src/features/weather/components/weather-content/components/CityWeatherOverview.tsx
@@ -20,25 +20,44 @@ export function CityWeatherOverview({ weatherForecast }: CityWeatherOverviewProp
     const date = getCurrentDate();
 
     const city = location?.address.city_district ? location.address.city_district : location?.address.village;
+    const country = location?.address.country;
 
     console.log(weatherForecast);
 
     return (
         <div className="relative">
-            <img
-                className="w-full h-full object-cover rounded-[var(--radius-24)]"
-                src={bgHeroImage}
-                alt="bg-hero-desktop-image"
-            />
-            <div className="absolute flex justify-between items-center top-0 bottom-0 left-0 right-0 p-[var(--spacing-24)] max-[550px]:text-center max-[550px]:justify-center">
-                <div>
-                    <h1 className="text-(length:--fs-32) font-(weight:--fw-semi-bold) text-[var(--neutral-0)] max-[550px]:text-(length:--fs-20)">
-                        {`${city}, ${location?.address.country}`}
-                    </h1>
-                    <span className="text-[var(--neutral-0)] max-[550px]:text-(length:--fs-14)">{date}</span>
-                </div>
-                <div></div>
-            </div>
+            <CityWeatherOverviewHeroImage />
+            <CityWeatherOverviewContent city={city} country={country} date={date} />
         </div>
     );
+}
+
+function CityWeatherOverviewHeroImage() {
+    return (
+        <img
+            className="w-full h-full object-cover rounded-[var(--radius-24)]"
+            src={bgHeroImage}
+            alt="bg-hero-desktop-image"
+        />
+    )
+}
+
+interface CityWeatherOverviewContentProps {
+    city: string | undefined,
+    country: string | undefined,
+    date: string
+}
+
+function CityWeatherOverviewContent({ city, country, date }: CityWeatherOverviewContentProps) {
+    return (
+        <div
+            className="absolute flex justify-between items-center top-0 bottom-0 left-0 right-0 p-[var(--spacing-24)] max-[550px]:text-center max-[550px]:justify-center">
+            <div>
+                <h1 className="text-(length:--fs-32) font-(weight:--fw-semi-bold) text-[var(--neutral-0)] max-[550px]:text-(length:--fs-20)">
+                    {`${city}, ${country}`}
+                </h1>
+                <span className="text-[var(--neutral-0)] max-[550px]:text-(length:--fs-14)">{date}</span>
+            </div>
+        </div>
+    )
 }
