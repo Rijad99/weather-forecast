ce6be5cb15aa219fef793cc80943040e94f12c46
rsmajlov <rijad.smajlovic@dccs.eu>
2025-10-05 19:24:38 +0200

    Finish responsiveness for testing purposes on tablets, mobile phone #2

--- src/features/weather/components/weather-content/components/city-base-weather-information/TodayWeatherSpecificationCard.tsx
+++ src/features/weather/components/weather-content/components/city-base-weather-information/TodayWeatherSpecificationCard.tsx
@@ -10,8 +10,8 @@ interface TodayWeatherSpecificationCardProps {
 export function TodayWeatherSpecificationCard({ label, value, unit }: TodayWeatherSpecificationCardProps) {
     return (
         <Card additionalClasses="flex flex-col justify-between gap-y-[var(--spacing-20)] pl-[var(--spacing-16)]">
-            <span className="text-[var(--neutral-0)] max-[900px]:text-(length:--fs-14) whitespace-nowrap">{label}</span>
-            <span className="text-[var(--neutral-0)] text-(length:--fs-28) max-[900px]:text-(length:--fs-20) whitespace-nowrap">{`${value}${unit}`}</span>
+            <span className="text-[var(--neutral-0)] max-[900px]:text-(length:--fs-14) max-[550px]:text-(length:--fs-12) whitespace-nowrap">{label}</span>
+            <span className="text-[var(--neutral-0)] text-(length:--fs-28) max-[900px]:text-(length:--fs-18) whitespace-nowrap">{`${value}${unit}`}</span>
         </Card>
     );
 }
