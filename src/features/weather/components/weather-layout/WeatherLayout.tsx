9854bb8a04e0e802bf131601998f3167b03c3057
rsmajlov <rijad.smajlovic@dccs.eu>
2025-10-06 12:49:15 +0200

    Adapt responsiveness for vertically larger screen resolutions

--- src/features/weather/components/weather-layout/WeatherLayout.tsx
+++ src/features/weather/components/weather-layout/WeatherLayout.tsx
@@ -13,11 +13,14 @@ interface WeatherLayoutProps {
 
 export function WeatherLayout({ weatherForecast }: WeatherLayoutProps) {
     return (
-        <div className="grid max-[700px]:grid-rows-[45px_auto_auto_1fr] grid-rows-[45px_auto_50px_1fr] gap-[var(--spacing-64)] h-screen px-[var(--spacing-112)] max-[900px]:px-[var(--spacing-16)] pt-[var(--spacing-48)] min-[1920px]:max-w-[1920px] min-[1920px]:m-auto">
-            <Header />
-            <SubHeader />
-            <Search />
-            <WeatherContent weatherForecast={weatherForecast} />
+        <div className="h-screen [@media(min-height:1080px)]:[place-content:center_center]">
+            <div
+                className="grid max-[700px]:grid-rows-[45px_auto_auto_1fr] grid-rows-[45px_auto_50px_1fr] gap-[var(--spacing-64)] h-screen [@media(min-height:1080px)_and_(min-width:1480px)]:h-[1080px] px-[var(--spacing-112)] [@media(min-height:1080px)_and_(min-width:1480px)]:pt-0 max-[900px]:px-[var(--spacing-16)] pt-[var(--spacing-48)] min-[1920px]:max-w-[1920px] min-[1920px]:m-auto">
+                <Header />
+                <SubHeader />
+                <Search />
+                <WeatherContent weatherForecast={weatherForecast} />
+            </div>
         </div>
     );
 }
