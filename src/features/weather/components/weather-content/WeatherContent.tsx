1fb54b56acc3c0f45a699645e08377f34f57007e
Rijad <rijad99.sm@gmail.com>
2025-10-06 18:15:52 +0200

    Fix CSS issue regarding responsiveness on small screens

--- src/features/weather/components/weather-content/WeatherContent.tsx
+++ src/features/weather/components/weather-content/WeatherContent.tsx
@@ -13,7 +13,7 @@ interface WeatherContentProps {
 
 export function WeatherContent({ weatherForecast }: WeatherContentProps) {
     return (
-        <div className="grid grid-cols-1 min-[1480px]:grid-cols-[2fr_1fr] gap-[var(--spacing-24)] pb-[var(--spacing-22)] [@media(max-height:1080px)]:max-h-[600px] [@media(max-height:1080px)]:pr-[var(--spacing-24)]">
+        <div className="grid grid-cols-1 min-[1480px]:grid-cols-[2fr_1fr] gap-[var(--spacing-24)] pb-[var(--spacing-22)] [@media(max-height:1080px)]:max-h-[600px]">
             <div className="grid grid-rows-[auto_auto_auto] min-[1300px]:grid-rows-[280px_auto_1fr] max-[550px]:grid-rows-[300px_auto_1fr] gap-[var(--spacing-24)] max-[550px]:gap-[var(--spacing-48)]">
                 <CityWeatherOverview weatherForecast={weatherForecast} />
                 <CityBaseWeatherInformation />
