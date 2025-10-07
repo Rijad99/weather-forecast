2d56a8076ddd2093ea58400333322a8ae35fe5b0
rsmajlov <rijad.smajlovic@dccs.eu>
2025-10-07 09:53:30 +0200

    Adapt responsiveness #2

--- src/features/weather/components/header/Header.tsx
+++ src/features/weather/components/header/Header.tsx
@@ -7,7 +7,7 @@ import { logoIcon } from '../../../../icons/icons';
 
 export function Header() {
     return (
-        <div className="flex items-center justify-between text-[var(--neutral-0)] text-(length:--fs-20) font">
+        <div className="flex items-center justify-between text-[var(--neutral-0)] text-(length:--fs-20) mt-[var(--spacing-48)]">
             <div className="flex items-center gap-[var(--spacing-16)]">
                 <Svg width="40" height="40" path={logoIcon} fill="var(--orange)" />
                 <span className="max-[550px]:text-(length:--fs-16) max-[500px]:text-(length:--fs-12)">Weather Now</span>
--- src/features/weather/components/weather-layout/WeatherLayout.tsx
+++ src/features/weather/components/weather-layout/WeatherLayout.tsx
@@ -14,7 +14,7 @@ interface WeatherLayoutProps {
 export function WeatherLayout({ weatherForecast }: WeatherLayoutProps) {
     return (
         <div className="h-screen [@media(min-height:1080px)]:[place-content:center_center]">
-            <div className="grid max-[700px]:grid-rows-[45px_auto_auto_1fr] grid-rows-[45px_auto_50px_1fr] gap-[var(--spacing-64)] h-screen [@media(min-height:1080px)_and_(min-width:1480px)]:h-[1080px] px-[var(--spacing-112)] [@media(min-height:1080px)_and_(min-width:1480px)]:pt-0 max-[900px]:px-[var(--spacing-16)] pt-[var(--spacing-48)] min-[1920px]:max-w-[1920px] min-[1920px]:m-auto">
+            <div className="grid max-[700px]:grid-rows-[45px_auto_auto_1fr] grid-rows-[45px_auto_50px_1fr] gap-[var(--spacing-64)] h-screen [@media(min-height:1080px)_and_(min-width:1480px)]:h-[1080px] px-[var(--spacing-112)] [@media(min-height:1080px)_and_(min-width:1480px)]:pt-0 max-[900px]:px-[var(--spacing-16)] min-[1920px]:max-w-[1920px] min-[1920px]:m-auto">
                 <Header />
                 <SubHeader />
                 <Search />
